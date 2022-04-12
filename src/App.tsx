e819b278f62515cd40c2f25dfc0252eef467dd14
Sara Noor <saranoor@wifi-roaming-128-4-152-187.wifi.udel.edu>
2022-04-12 14:04:07 -0400

    Finished Welcome Message

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import "./App.css";
+import { OpeningInstruction } from "./openingInstruction";
 
 function App(): JSX.Element {
     return (
         <div className="App">
             <header className="App-header">
-                UD CISC275 with React Hooks and TypeScript
+                <OpeningInstruction></OpeningInstruction>
             </header>
             <p>Sara Noor, Riley Johnson, and Sydney Segear</p>
             <p>
