e29f1460da4621c3ac8d28170296c8aeafbdb6e2
Sydney Segear <sydneysegear@wifi-roaming-128-4-132-222.wifi.udel.edu>
2022-04-19 14:56:32 -0400

    Welcome message tests

--- src/Welcome.test.tsx
+++ src/Welcome.test.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { queryAllByText, render, screen } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 //import userEvent from "@testing-library/user-event";
 import { OpeningInstruction } from "./openingInstruction";
 
