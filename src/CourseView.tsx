2969f37893c7a0a46ead75486f8d2d9c9b148502
Sara Noor <saranoor@wifi-roaming-128-4-152-156.wifi.udel.edu>
2022-04-19 14:39:16 -0400

    add course droplist

--- src/CourseView.tsx
+++ src/CourseView.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { Form } from "react-bootstrap";
 import { Course } from "./interfaces/Course";
-import ClassData from "./ClassData.json";
+import catalog2 from "./catalog2.json";
 import { CourseDisplay } from "./CourseDisplay";
 
 type ChangeEvent = React.ChangeEvent<
@@ -9,16 +9,29 @@ type ChangeEvent = React.ChangeEvent<
 >;
 
 export function CourseView(): JSX.Element {
-    const [chosenCourse, setChosenCourse] = useState<string>(
-        ClassData[0].courseTitle
-    );
+    const [chosenCourse, setChosenCourse] = useState<string>(catalog2[0].code);
 
     function updateCourse(event: ChangeEvent) {
         setChosenCourse(event.target.value);
+        console.log(courseInfo);
     }
+    const courseInfo = catalog2.find(
+        (course: Course): boolean => course.code == chosenCourse
+    );
     return (
         <div>
             <h3>Courses</h3>
+            <Form.Group controlId="chosenClass">
+                <Form.Label>Choose a Course</Form.Label>
+                <Form.Select value={chosenCourse} onChange={updateCourse}>
+                    {catalog2.map((course: Course) => (
+                        <option key={course.code} value={course.code}>
+                            {course.name}
+                        </option>
+                    ))}
+                </Form.Select>
+            </Form.Group>
+            <CourseDisplay chosenCourse={courseInfo}></CourseDisplay>
         </div>
     );
 }
